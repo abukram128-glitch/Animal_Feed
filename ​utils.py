5fdd1ced5a4e4dc4e088aa1bc23c3a32de0d1fc1
abukram128-glitch <abukram128@gmail.com>
2026-05-24 11:47:25 +0200

    Update ​utils.py

--- ​utils.py
+++ ​utils.py
@@ -1,14 +1,10 @@
 # utils.py
 import streamlit as st
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
-def send_code_to_mail(receiver_email, sender_email, sender_password, code_content):
-    """إرسال السورس كود للمالك فقط"""
-    # (ضع هنا منطق دالة الإرسال الخاص بك مع استخدام st.secrets لكلمة السر)
+def send_code_to_mail(receiver_email, code):
+    # انسخ كود الإرسال البريدي الخاص بك
     pass
 
-def generate_pdf_report(formula, target_cp, breed, cost, city):
-    # (كود توليد الـ PDF)
+def generate_pdf_report(formula_data):
+    # انسخ كود الـ PDF الخاص بك
     pass
